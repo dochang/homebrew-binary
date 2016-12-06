451c67d318b594adca83ecbc4689cc0b74687b6b
Desmond O. Chang <dochang@gmail.com>
2016-12-07 07:44:06 +0800

    Add Arduino formula

---
+++ Formula/arduino.rb
@@ -0,0 +1,39 @@
+CHECKSUMS = {
+  linux64: "492b28d72b347227346592ceb0373af55558aab67acda343a8a15cc11ade154a",
+}.freeze
+
+class Arduino < Formula
+  desc "Arduino IDE"
+  homepage "https://www.arduino.cc/"
+  version "1.6.13"
+
+  class << self
+    def platform
+      case
+      when OS.linux?
+        case
+        when Hardware::CPU.intel?
+          if Hardware::CPU.is_64_bit?
+            return :linux64
+          end
+        end
+      end
+      raise "Platform not supported."
+    end
+
+    def extname
+      platform == :linux ? "tar.xz" : "zip"
+    end
+  end
+
+  url "https://downloads.arduino.cc/arduino-#{version}-#{platform}.#{extname}"
+  sha256 CHECKSUMS[platform]
+
+  def install
+    prefix.install Dir["*"]
+  end
+
+  test do
+    system "false"
+  end
+end
