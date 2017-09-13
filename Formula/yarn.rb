1e22227c2afd3ca77d533c46adcb36a990b2d613
Desmond O. Chang <dochang@gmail.com>
2017-09-13 11:02:49 +0800

    yarn 1.0.1

--- Formula/yarn.rb
+++ Formula/yarn.rb
@@ -1,11 +1,11 @@
-VERSION = "0.27.5".freeze
+VERSION = "1.0.1".freeze
 
 class Yarn < Formula
   desc "Javascript package manager"
   homepage "https://yarnpkg.com/"
 
   url "https://yarnpkg.com/downloads/#{VERSION}/yarn-v#{VERSION}.tar.gz"
-  sha256 "f0f3510246ee74eb660ea06930dcded7b684eac2593aa979a7add84b72517968"
+  sha256 "6b00b5e0a7074a512d39d2d91ba6262dde911d452617939ca4be4a700dd77cf1"
 
   def install
     prefix.install Dir["*"]
