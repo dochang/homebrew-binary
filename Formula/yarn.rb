458abe9f92e88179d881bc4428742e33f124b033
Desmond O. Chang <dochang@gmail.com>
2017-10-05 16:56:56 +0800

    yarn 1.1.0

--- Formula/yarn.rb
+++ Formula/yarn.rb
@@ -2,8 +2,8 @@ class Yarn < Formula
   desc "Javascript package manager"
   homepage "https://yarnpkg.com/"
 
-  url "https://yarnpkg.com/downloads/1.0.1/yarn-v1.0.1.tar.gz"
-  sha256 "6b00b5e0a7074a512d39d2d91ba6262dde911d452617939ca4be4a700dd77cf1"
+  url "https://yarnpkg.com/downloads/1.1.0/yarn-v1.1.0.tar.gz"
+  sha256 "171c1f9ee93c488c0d774ac6e9c72649047c3f896277d88d0f805266519430f3"
 
   def install
     prefix.install Dir["*"]
