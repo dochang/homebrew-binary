eca22752c24bcae6bf4f82ee0fa06f0c65583917
Desmond O. Chang <dochang@gmail.com>
2017-10-05 16:47:50 +0800

    rclone 1.38

--- Formula/rclone.rb
+++ Formula/rclone.rb
@@ -7,9 +7,9 @@ class Rclone < Formula
     case
     when Hardware::CPU.intel?
       if Hardware::CPU.is_64_bit?
-        url "https://downloads.rclone.org/rclone-v1.37-linux-amd64.zip"
-        version "1.37"
-        sha256 "a0886f5e907cd19728d4c37699db0a3ea4b7e3f4e071c39883446b3322ac5617"
+        url "https://downloads.rclone.org/rclone-v1.38-linux-amd64.zip"
+        version "1.38"
+        sha256 "69bcd262f6e67eaa9f28f93460d73ad8a73612745d3c7a2bdd03d4cf85d43090"
       end
     end
   end
