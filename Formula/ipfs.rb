d1177072f2136509d16ef1fbf91c02696c028a45
Desmond O. Chang <dochang@gmail.com>
2017-10-05 16:35:42 +0800

    ipfs 0.4.11

--- Formula/ipfs.rb
+++ Formula/ipfs.rb
@@ -7,9 +7,9 @@ class Ipfs < Formula
     case
     when Hardware::CPU.intel?
       if Hardware::CPU.is_64_bit?
-        url "https://dist.ipfs.io/go-ipfs/v0.4.10/go-ipfs_v0.4.10_linux-amd64.tar.gz"
-        version "0.4.10"
-        sha256 "c9d4ee36c3d8ea84c56243e63ee4873f43b0fca77d5afa5053ef2c21f286e9cb"
+        url "https://dist.ipfs.io/go-ipfs/v0.4.11/go-ipfs_v0.4.11_linux-amd64.tar.gz"
+        version "0.4.11"
+        sha256 "a638ac436597ba20b1a25b289b8c89cfa31a1fc7399de66a35ba04e45562b482"
       end
     end
   end
