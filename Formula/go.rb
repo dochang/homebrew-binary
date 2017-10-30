2e8b95a6cc0e61af45fed956ea380ae559279ca7
Desmond O. Chang <dochang@gmail.com>
2017-10-31 01:36:34 +0800

    go 1.9.2

--- Formula/go.rb
+++ Formula/go.rb
@@ -7,9 +7,9 @@ class Go < Formula
     case
     when Hardware::CPU.intel?
       if Hardware::CPU.is_64_bit?
-        url "https://storage.googleapis.com/golang/go1.9.1.linux-amd64.tar.gz"
-        version "1.9.1"
-        sha256 "07d81c6b6b4c2dcf1b5ef7c27aaebd3691cdb40548500941f92b221147c5d9c7"
+        url "https://storage.googleapis.com/golang/go1.9.2.linux-amd64.tar.gz"
+        version "1.9.2"
+        sha256 "de874549d9a8d8d8062be05808509c09a88a248e77ec14eb77453530829ac02b"
       end
     end
   end
