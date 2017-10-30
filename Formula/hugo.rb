4563ad2a80fcfd5e12b31dd9c59916ec8e223747
Desmond O. Chang <dochang@gmail.com>
2017-10-31 01:41:07 +0800

    hugo 0.30.2

--- Formula/hugo.rb
+++ Formula/hugo.rb
@@ -7,9 +7,9 @@ class Hugo < Formula
     case
     when Hardware::CPU.intel?
       if Hardware::CPU.is_64_bit?
-        url "https://github.com/gohugoio/hugo/releases/download/v0.29/hugo_0.29_Linux-64bit.tar.gz"
-        version "0.29"
-        sha256 "80011b40b64781a5adfbf882fe3ab3b3c526e9ba8583fb532cd2331ffbe44a9e"
+        url "https://github.com/gohugoio/hugo/releases/download/v0.30.2/hugo_0.30.2_Linux-64bit.tar.gz"
+        version "0.30.2"
+        sha256 "a192577471f2c5b7a6f26ce8ec6effd9e274ffb8672c1a810af0a6384b4de8cd"
       end
     end
   end
