c4eda78d213c6498a2c19070917df6b87a33e6f5
Desmond O. Chang <dochang@gmail.com>
2018-02-11 00:56:46 +0800

    terraform 0.11.3

--- Formula/terraform.rb
+++ Formula/terraform.rb
@@ -7,9 +7,9 @@ class Terraform < Formula
     case
     when Hardware::CPU.intel?
       if Hardware::CPU.is_64_bit?
-        url "https://releases.hashicorp.com/terraform/0.11.1/terraform_0.11.1_linux_amd64.zip"
-        version "0.11.1"
-        sha256 "4e3d5e4c6a267e31e9f95d4c1b00f5a7be5a319698f0370825b459cb786e2f35"
+        url "https://releases.hashicorp.com/terraform/0.11.3/terraform_0.11.3_linux_amd64.zip"
+        version "0.11.3"
+        sha256 "6b8a7b83954597d36bbed23913dd51bc253906c612a070a21db373eab71b277b"
       end
     end
   end
