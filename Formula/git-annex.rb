eebbfc7b2d954cbc358d02f20e8d220e0b1db136
Desmond O. Chang <dochang@gmail.com>
2018-03-18 07:44:14 +0800

    git-annex 6.20180227

--- Formula/git-annex.rb
+++ Formula/git-annex.rb
@@ -7,12 +7,12 @@ class GitAnnex < Formula
       case Hardware::CPU.arch
       when :x86_64
         url "https://downloads.kitenet.net/git-annex/linux/current/git-annex-standalone-amd64.tar.gz"
-        version "6.20180112"
-        sha256 "ce6a7ce9167c6e0690277696bc7657bc9c6476a837819c6cd5ad4d2bc3a9eaed"
+        version "6.20180227"
+        sha256 "f5b4acd2a7ec49b7644c311eba7097567cbd8df23175b1c8cab08a4076d0a8c8"
       when :arm
         url "https://downloads.kitenet.net/git-annex/linux/current/git-annex-standalone-armel.tar.gz"
-        version "6.20180112"
-        sha256 "eec0be47b49af4cadb3ebd67bda5fd26da3f5c9724ba4ca1e025363b051eeaf7"
+        version "6.20180227"
+        sha256 "b2268c08d611984adce7bff30c5ebd5e88bac47cf48703034e26660a6405f54a"
       end
     end
   end
