40ec12a873bb2ad8f1499d356423b87a72ec08f8
Desmond O. Chang <dochang@gmail.com>
2018-04-03 16:40:43 +0800

    doctl 1.7.2

--- Formula/doctl.rb
+++ Formula/doctl.rb
@@ -6,12 +6,12 @@ class Doctl < Formula
     if OS.linux?
       case Hardware::CPU.arch
       when :x86_64
-        url "https://github.com/digitalocean/doctl/releases/download/v1.7.1/doctl-1.7.1-linux-amd64.tar.gz"
-        version "1.7.1"
-        sha256 "48ce63e156f65922f73678011279cf6d13e0971a76fcc87a3a7ff17eed33db78"
+        url "https://github.com/digitalocean/doctl/releases/download/v1.7.2/doctl-1.7.2-linux-amd64.tar.gz"
+        version "1.7.2"
+        sha256 "4cc1ac258e0cb8f7325cbcd4fd9796376e57cd71a3323886ffc018c6f8d4baf1"
       when :arm
         url "file://#{__FILE__}"
-        version "1.7.1"
+        version "1.7.2"
       end
     end
   end
