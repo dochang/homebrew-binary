c82f0582990bb55d682195fffd5c7d9ac4cdfd8b
Desmond O. Chang <dochang@gmail.com>
2018-04-03 17:58:54 +0800

    ripgrep 0.8.1

--- Formula/ripgrep.rb
+++ Formula/ripgrep.rb
@@ -6,12 +6,12 @@ class Ripgrep < Formula
     if OS.linux?
       case Hardware::CPU.arch
       when :x86_64
-        url "https://github.com/BurntSushi/ripgrep/releases/download/0.8.0/ripgrep-0.8.0-x86_64-unknown-linux-musl.tar.gz"
-        sha256 "621f2f16f65203aa37e7c10ecfb22384c4c01e70ebbd30a13c0d6944ffc6e59e"
+        url "https://github.com/BurntSushi/ripgrep/releases/download/0.8.1/ripgrep-0.8.1-x86_64-unknown-linux-musl.tar.gz"
+        sha256 "08b1aa1440a23a88c94cff41a860340ecf38e9108817aff30ff778c00c63eb76"
       when :arm
-        url "https://github.com/BurntSushi/ripgrep/releases/download/0.8.0/ripgrep-0.8.0-arm-unknown-linux-gnueabihf.tar.gz"
-        version "0.8.0"
-        sha256 "1faaaaa6f4583d751e10333e0c706eac77f8ab8ee0605d85d25c09785c5d97bc"
+        url "https://github.com/BurntSushi/ripgrep/releases/download/0.8.1/ripgrep-0.8.1-arm-unknown-linux-gnueabihf.tar.gz"
+        version "0.8.1"
+        sha256 "211414cdfed32b27479a14dd21e49d11fa02a24fb396051ef53af67b9141b3cd"
       end
     end
   end
