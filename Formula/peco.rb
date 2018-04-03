fa74f9489232d9168ad4115f667ffced7e8a86c5
Desmond O. Chang <dochang@gmail.com>
2018-04-03 17:52:07 +0800

    peco 0.5.3

--- Formula/peco.rb
+++ Formula/peco.rb
@@ -6,13 +6,13 @@ class Peco < Formula
     if OS.linux?
       case Hardware::CPU.arch
       when :x86_64
-        url "https://github.com/peco/peco/releases/download/v0.5.2/peco_linux_amd64.tar.gz"
-        version "0.5.2"
-        sha256 "3dfb85c44c68cd10584c9813f96bd979ae1118735819eda9588cc4ef8adcb99f"
+        url "https://github.com/peco/peco/releases/download/v0.5.3/peco_linux_amd64.tar.gz"
+        version "0.5.3"
+        sha256 "a64610d163af8370dc1d61dc57daae36a1b3ba98b40f4096898a0b85f7e82c58"
       when :arm
-        url "https://github.com/peco/peco/releases/download/v0.5.2/peco_linux_arm.tar.gz"
-        version "0.5.2"
-        sha256 "ccb179e53f15d4b230bb4d40d822e9896d63d855126cab0277164710d8c8ea23"
+        url "https://github.com/peco/peco/releases/download/v0.5.3/peco_linux_arm.tar.gz"
+        version "0.5.3"
+        sha256 "6aec41f821eb5add2c035fecc6b3865ed933ff3055f4ee15ed78d27fee0dbf5f"
       end
     end
   end
