8ed5c86bb938ade7c1d473835c33889e7989c562
Desmond O. Chang <dochang@gmail.com>
2018-04-03 17:22:34 +0800

    go 1.10.1

--- Formula/go.rb
+++ Formula/go.rb
@@ -6,13 +6,13 @@ class Go < Formula
     if OS.linux?
       case Hardware::CPU.arch
       when :x86_64
-        url "https://dl.google.com/go/go1.10.linux-amd64.tar.gz"
-        version "1.10"
-        sha256 "b5a64335f1490277b585832d1f6c7f8c6c11206cba5cd3f771dcb87b98ad1a33"
+        url "https://dl.google.com/go/go1.10.1.linux-amd64.tar.gz"
+        version "1.10.1"
+        sha256 "72d820dec546752e5a8303b33b009079c15c2390ce76d67cf514991646c6127b"
       when :arm
-        url "https://dl.google.com/go/go1.10.linux-armv6l.tar.gz"
-        version "1.10"
-        sha256 "6ff665a9ab61240cf9f11a07e03e6819e452a618a32ea05bbb2c80182f838f4f"
+        url "https://dl.google.com/go/go1.10.1.linux-armv6l.tar.gz"
+        version "1.10.1"
+        sha256 "feca4e920d5ca25001dc0823390df79bc7ea5b5b8c03483e5a2c54f164654936"
       end
     end
   end
