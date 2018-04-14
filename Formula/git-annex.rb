378558807b577e60f7665dc123273de3475c6710
Desmond O. Chang <dochang@gmail.com>
2018-04-14 12:49:21 +0800

    git-annex 6.20180409

--- Formula/git-annex.rb
+++ Formula/git-annex.rb
@@ -10,21 +10,34 @@ class GitAnnex < Formula
       case Hardware::CPU.arch
       when :x86_64
         url "https://downloads.kitenet.net/git-annex/linux/current/git-annex-standalone-amd64.tar.gz"
-        version "6.20180316"
-        sha256 "f5b4acd2a7ec49b7644c311eba7097567cbd8df23175b1c8cab08a4076d0a8c8"
+        version "6.20180409"
+        sha256 "645e7cb099ae1699e0a399fc47d7ef6276e48017820d92cff0959d777d261bb7"
       when :arm
         url "https://downloads.kitenet.net/git-annex/linux/current/git-annex-standalone-armel.tar.gz"
-        version "6.20180316"
-        sha256 "b2268c08d611984adce7bff30c5ebd5e88bac47cf48703034e26660a6405f54a"
+        version "6.20180409"
+        sha256 "0b1779cdd8e162f910e68c17693f90422a0c734c987b0b6f02cc7ad8ca104fd7"
       end
     end
   end
 
+  def pkgname
+    "git-annex.linux"
+  end
+
+  # Put these in a method to fix RuboCop [Metrics/AbcSize][1] error
+  #
+  # [1]: https://rubocop.readthedocs.io/en/latest/cops_metrics/#metricsabcsize
+  def install_bin
+    bin.install_symlink "../opt/#{pkgname}/git-annex"
+    bin.install_symlink "../opt/#{pkgname}/git-annex-shell"
+    bin.install_symlink "../opt/#{pkgname}/git-annex" => "git-remote-tor-annex"
+  end
+
   def install
-    bin.install "shimmed/git-annex/git-annex"
-    bin.install_symlink "git-annex" => "git-annex-shell"
-    bin.install_symlink "git-annex" => "git-remote-tor-annex"
-    man.install "usr/share/man/man1"
+    (prefix/"opt"/pkgname).install Dir["*"]
+    install_bin
+    # No need to install man pages, since git-annex is a shell wrapper that
+    # overrides related variables.
   end
 
   test do
