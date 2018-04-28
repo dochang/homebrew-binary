36992e8adf455ebe4e259b74b7783028c3e056d2
Desmond O. Chang <dochang@gmail.com>
2018-04-28 20:03:14 +0800

    hugo 0.40.1

--- Formula/hugo.rb
+++ Formula/hugo.rb
@@ -6,13 +6,13 @@ class Hugo < Formula
     if OS.linux?
       case Hardware::CPU.arch
       when :x86_64
-        url "https://github.com/gohugoio/hugo/releases/download/v0.38.2/hugo_0.38.2_Linux-64bit.tar.gz"
-        version "0.38.2"
-        sha256 "9e5b7cd79e4732c4fdf82210450e39cc588935fdc8ecf4a590219d7b4b2a389a"
+        url "https://github.com/gohugoio/hugo/releases/download/v0.40.1/hugo_0.40.1_Linux-64bit.tar.gz"
+        version "0.40.1"
+        sha256 "004af6326a331f7e0e2ae2145a4263e36c9a15cb9dcf1d0d9dac4bbdf6d678d5"
       when :arm
-        url "https://github.com/gohugoio/hugo/releases/download/v0.38.2/hugo_0.38.2_Linux-ARM.tar.gz"
-        version "0.38.2"
-        sha256 "9878b7cb91acc29ea6785604775c68ea8e95a677aec4755596a6959762cc1c69"
+        url "https://github.com/gohugoio/hugo/releases/download/v0.40.1/hugo_0.40.1_Linux-ARM.tar.gz"
+        version "0.40.1"
+        sha256 "c77690927867caccc2ba1d33443161c7af732dc9622a23aee95d6dc60c6a2851"
       end
     end
   end
