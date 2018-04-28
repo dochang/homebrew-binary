20095a7b83a20356d40b5f7eafb522e0f9017d44
Desmond O. Chang <dochang@gmail.com>
2018-04-28 20:01:09 +0800

    haskell-stack 1.7.1

--- Formula/haskell-stack.rb
+++ Formula/haskell-stack.rb
@@ -6,18 +6,16 @@ class HaskellStack < Formula
     if OS.linux?
       case Hardware::CPU.arch
       when :x86_64
-        url "https://github.com/commercialhaskell/stack/releases/download/v1.6.5/stack-1.6.5-linux-x86_64-static.tar.gz"
-        sha256 "f8876fa73c4bb95c1d762073af2dfa4a003d795709ec3fee2f9743ca6a5fedc6"
+        url "https://github.com/commercialhaskell/stack/releases/download/v1.7.1/stack-1.7.1-linux-x86_64.tar.gz"
+        sha256 "647306bbf65dd0b555486e2a32776ba1cfe0c8f380e31c6aa93abd452cb209ee"
       when :arm
-        url "file://#{__FILE__}"
-        version "1.6.5"
+        url "https://github.com/commercialhaskell/stack/releases/download/v1.7.1/stack-1.7.1-linux-arm.tar.gz"
+        sha256 "0d79990b7d38c41203a031c842938c7fdc280f5d5d4f411cd91c7c53609eff76"
       end
     end
   end
 
   def install
-    odie "Platform not supported." if active_spec.url == "file://#{__FILE__}"
-
     bin.install "stack"
   end
 
