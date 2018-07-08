61a6ec3f728c88840ff74535a5df04997154bfba
Desmond O. Chang <dochang@gmail.com>
2018-07-08 16:08:10 +0800

    rclone 1.42

--- Formula/rclone.rb
+++ Formula/rclone.rb
@@ -6,13 +6,13 @@ class Rclone < Formula
     if OS.linux?
       case Hardware::CPU.arch
       when :x86_64
-        url "https://downloads.rclone.org/v1.41/rclone-v1.41-linux-amd64.zip"
-        version "1.41"
-        sha256 "ae8dee585c013ac0b64dee22cf788d9458f44c536a8edcc45d25f091703c3f2e"
+        url "https://downloads.rclone.org/v1.42/rclone-v1.42-linux-amd64.zip"
+        version "1.42"
+        sha256 "7a623f60a5995f33cca3ed285210d8701c830f6f34d4dc50d74d75edd6a5bfa6"
       when :arm
-        url "https://downloads.rclone.org/v1.41/rclone-v1.41-linux-arm.zip"
-        version "1.41"
-        sha256 "91e3a747c64a1f15920cf9a7ea5a59f41d222810eeebaeec186fdefabe96cb20"
+        url "https://downloads.rclone.org/v1.42/rclone-v1.42-linux-arm.zip"
+        version "1.42"
+        sha256 "24e4e253176938af5c29553c23bbdc240d84af912014cbad0e7a447d55c1faf6"
       end
     end
   end
