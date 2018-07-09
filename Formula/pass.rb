e03194f24021a895bb2eb8bbf680763bcff22eaa
Desmond O. Chang <dochang@gmail.com>
2018-07-09 15:33:30 +0800

    Remove revision spec

--- Formula/pass.rb
+++ Formula/pass.rb
@@ -3,7 +3,6 @@ class Pass < Formula
   homepage "https://www.passwordstore.org/"
   url "https://git.zx2c4.com/password-store/snapshot/password-store-1.7.2.tar.xz"
   sha256 "4768c5e1965c4d2aeb28818681e484fb105b6f46cbd75a97608615c4ec6980ea"
-  revision 1
   head "https://git.zx2c4.com/password-store", :using => :git
 
   def install
