ebe65c293fcf9a19f2b03b19261cbcbed7e16277
Desmond O. Chang <dochang@gmail.com>
2018-08-07 05:02:38 +0800

    packer 1.2.5

--- Formula/packer.rb
+++ Formula/packer.rb
@@ -6,19 +6,18 @@ class Packer < Formula
     if OS.linux?
       case Hardware::CPU.arch
       when :x86_64
-        url "https://releases.hashicorp.com/packer/1.2.4/packer_1.2.4_linux_amd64.zip"
-        version "1.2.4"
-        sha256 "258d1baa23498932baede9b40f2eca4ac363b86b32487b36f48f5102630e9fbb"
+        url "https://releases.hashicorp.com/packer/1.2.5/packer_1.2.5_linux_amd64.zip"
+        version "1.2.5"
+        sha256 "bc58aa3f3db380b76776e35f69662b49f3cf15cf80420fc81a15ce971430824c"
       when :arm
-        url "file://#{__FILE__}"
-        version "1.2.4"
+        url "https://releases.hashicorp.com/packer/1.2.5/packer_1.2.5_linux_arm.zip"
+        version "1.2.5"
+        sha256 "c5b305707042d441e4d8a291c2c38cfc35d4333c53a7311e27ab424a310b205f"
       end
     end
   end
 
   def install
-    odie "Platform not supported." if active_spec.url == "file://#{__FILE__}"
-
     bin.install "packer"
   end
 
