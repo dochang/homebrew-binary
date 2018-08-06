c08383859133ff64cfc8215f4511f286ea646fde
Desmond O. Chang <dochang@gmail.com>
2018-08-07 04:57:58 +0800

    ipfs 0.4.17

--- Formula/ipfs.rb
+++ Formula/ipfs.rb
@@ -6,13 +6,13 @@ class Ipfs < Formula
     if OS.linux?
       case Hardware::CPU.arch
       when :x86_64
-        url "https://dist.ipfs.io/go-ipfs/v0.4.15/go-ipfs_v0.4.15_linux-amd64.tar.gz"
-        version "0.4.15"
-        sha256 "48a81cfc34d3a12c8563dbdfae8681be6e4d23c0664d6a192bc2758c4e4ef377"
+        url "https://dist.ipfs.io/go-ipfs/v0.4.17/go-ipfs_v0.4.17_linux-amd64.tar.gz"
+        version "0.4.17"
+        sha256 "1bb1050ebe74f9042ac721eb1b71f92e1b0e78e54c59dadfde13080facb2901c"
       when :arm
-        url "https://dist.ipfs.io/go-ipfs/v0.4.15/go-ipfs_v0.4.15_linux-arm.tar.gz"
-        version "0.4.15"
-        sha256 "daa390ab416335a3687a0332ce9ab67c45221dd3b11c6e26ef6c7a99623fb13a"
+        url "https://dist.ipfs.io/go-ipfs/v0.4.17/go-ipfs_v0.4.17_linux-arm.tar.gz"
+        version "0.4.17"
+        sha256 "893461d001a7e8709c9ec6983e0e3e8df7f710f8b8108db1185ed61f46a9a6de"
       end
     end
   end
