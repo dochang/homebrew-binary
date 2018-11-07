416606fdff137d1499893c40b620ff8359aa9bb5
Desmond O. Chang <dochang@gmail.com>
2018-11-07 15:27:22 +0800

    restic 0.9.3

--- Formula/restic.rb
+++ Formula/restic.rb
@@ -6,13 +6,13 @@ class Restic < Formula
     if OS.linux?
       case Hardware::CPU.arch
       when :x86_64
-        url "https://github.com/restic/restic/releases/download/v0.9.2/restic_0.9.2_linux_amd64.bz2"
-        version "0.9.2"
-        sha256 "1ad8a76a9f966da5f7a319c49a6db071a60ebaa24d69e6d86d53d6f2bcaf11ed"
+        url "https://github.com/restic/restic/releases/download/v0.9.3/restic_0.9.3_linux_amd64.bz2"
+        version "0.9.3"
+        sha256 "3c882962fc07f611a6147ada99c9909770d3e519210fd483cde9609c6bdd900c"
       when :arm
-        url "https://github.com/restic/restic/releases/download/v0.9.2/restic_0.9.2_linux_arm.bz2"
-        version "0.9.2"
-        sha256 "40d9fe8c9f191ab5d0f3e172eadac4fb3aef7a698b895a22ce81102b0a0f270a"
+        url "https://github.com/restic/restic/releases/download/v0.9.3/restic_0.9.3_linux_arm.bz2"
+        version "0.9.3"
+        sha256 "870d0643bce12a91a51947e9fee61b2ccd3b0fd12c21e81bcfcdfd6248f4c287"
       end
     end
   end
