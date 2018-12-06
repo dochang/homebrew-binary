c28406b9c37adb859ef64d83f8c1fb204ecccb26
Desmond O. Chang <dochang@gmail.com>
2018-12-06 18:51:33 +0800

    haskell-stack 1.9.3

--- Formula/haskell-stack.rb
+++ Formula/haskell-stack.rb
@@ -6,11 +6,12 @@ class HaskellStack < Formula
     if OS.linux?
       case Hardware::CPU.arch
       when :x86_64
-        url "https://github.com/commercialhaskell/stack/releases/download/v1.9.1/stack-1.9.1-linux-x86_64-static.tar.gz"
-        sha256 "332e5d0421e7a1afc96985050d985c0d2ba651f91dc2565faa5abfdef9d2a095"
+        url "https://github.com/commercialhaskell/stack/releases/download/v1.9.3/stack-1.9.3-linux-x86_64-static.tar.gz"
+        version "1.9.3"
+        sha256 "c9bf6d371b51de74f4bfd5b50965966ac57f75b0544aebb59ade22195d0b7543"
       when :arm
-        url "https://github.com/commercialhaskell/stack/releases/download/v1.9.1/stack-1.9.1-linux-arm.tar.gz"
-        sha256 "c400d6403d6d138754056327465cec7ef113d75b5d7ee26e9476268ca1dd946f"
+        url "https://github.com/commercialhaskell/stack/releases/download/v1.9.3/stack-1.9.3-linux-arm.tar.gz"
+        sha256 "96394b616e29b3bd2479a3712e2c0c375a7cfd748b613f7c6c3186d7ae92977b"
       end
     end
   end
