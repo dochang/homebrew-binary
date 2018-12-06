7c5145944e4734156eb27a8568289d3e82af42c4
Desmond O. Chang <dochang@gmail.com>
2018-12-06 19:03:13 +0800

    packer 1.3.3

--- Formula/packer.rb
+++ Formula/packer.rb
@@ -6,13 +6,13 @@ class Packer < Formula
     if OS.linux?
       case Hardware::CPU.arch
       when :x86_64
-        url "https://releases.hashicorp.com/packer/1.3.2/packer_1.3.2_linux_amd64.zip"
-        version "1.3.2"
-        sha256 "5e51808299135fee7a2e664b09f401b5712b5ef18bd4bad5bc50f4dcd8b149a1"
+        url "https://releases.hashicorp.com/packer/1.3.3/packer_1.3.3_linux_amd64.zip"
+        version "1.3.3"
+        sha256 "efa311336db17c0709d5069509c34c35f0d59c63dfb05f61d4572c5a26b563ea"
       when :arm
-        url "https://releases.hashicorp.com/packer/1.3.2/packer_1.3.2_linux_arm.zip"
-        version "1.3.2"
-        sha256 "1ec7416ebf6170c34b4005363eaed5ae8611b7816f0806c36ed17602b0661622"
+        url "https://releases.hashicorp.com/packer/1.3.3/packer_1.3.3_linux_arm.zip"
+        version "1.3.3"
+        sha256 "1147211a92354f08733dfbbc8e9e9cc02a84773c4a10c97c76eafe8347d1d406"
       end
     end
   end
